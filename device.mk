e95219e982d5c1f982458ae81e31a933c222d2eb
Zinadin Zidan <zidan.roking@gmail.com>
2021-12-09 04:57:19 +0200

    RMX1921: Guard qti thermal 2.0 service hals * Its crashes on rmx1921 but fine on other devices
     * Crash:
        - E android.hardware.thermal@2.0-service.qti: ThermalHAL not initialized properly.
        - E ThermalHalWrapper: Couldn't get temperatures because of HAL error: ThermalHAL not initialized properly.

--- device.mk
+++ device.mk
@@ -546,8 +546,10 @@ PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
 # Thermal
+ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
+endif
 
 # USB
 PRODUCT_PACKAGES += \
